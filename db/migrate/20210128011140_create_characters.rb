6b1cf1149d64c8760c38a8f42d0e724629c2debc
Joana Bertoldi <joanasoares_1994@msn.com>
2021-02-11 23:59:39 +0000

    Add rspec. Remove null false from foreign_keys in characteres table.

--- db/migrate/20210128011140_create_characters.rb
+++ db/migrate/20210128011140_create_characters.rb
@@ -6,8 +6,8 @@ class CreateCharacters < ActiveRecord::Migration[6.0]
       t.string :epithet
       t.integer :bounty
       t.string :status, default: 'alive'
-      t.references :devil_fruit, null: false, foreign_key: true
-      t.references :pirate_crew, null: false, foreign_key: true
+      t.references :devil_fruit, foreign_key: true
+      t.references :pirate_crew, foreign_key: true
 
       t.timestamps
     end
