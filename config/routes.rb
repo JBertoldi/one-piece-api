8840132b1cd96a0018df01fe7fe0a142540e6a2d
Joana Bertoldi <joanasoares_1994@msn.com>
2021-02-14 00:03:03 +0000

    Add route and show action to characters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :characters, only: %i[index]
+      resources :characters, only: %i[index show]
     end
   end
 end
