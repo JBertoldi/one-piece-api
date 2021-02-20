598759cde6c526d30988c4ed8cb7104fc8622c1a
Joana Bertoldi <joanasoares_1994@msn.com>
2021-02-20 15:55:16 +0000

    Add pg_search document presenter

---
+++ app/presenters/base_presenter.rb
@@ -0,0 +1,3 @@
+class BasePresenter
+  include Rails.application.routes.url_helpers
+end
--- app/presenters/pg_search/document_presenter.rb
+++ app/presenters/pg_search/document_presenter.rb
@@ -2,5 +2,7 @@ module PgSearch
   class DocumentPresenter < BasePresenter
     related_to :searchable
     build_with :content, :searchable_id, :searchable_type
+
+    # TODO: add url in response.body
   end
 end
